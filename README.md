b16223f68b5d004bb2d8590957178a0f487545a1
root <jose@wazuh.com>
2016-07-28 10:05:59 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# wazuh-puppet
-Puppet recipes to install Wazuh Manager and Agent
+# OSSEC Puppet module
+
+This module installs and configure OSSEC HIDS agent and manager.
+
+## Documentation
+
+* [Full documentation](http://documentation.wazuh.com)
+* [OSSEC Puppet module documentation](http://documentation.wazuh.com/en/latest/ossec_puppet.html#ossec-puppet-module)
+* [Puppet Forge](https://forge.puppetlabs.com/wazuh/ossec)
+
+## Credits and thank you
+
+This Puppet module has been authored by Nicolas Zin, and updated by Jonathan Gazeley and Michael Porter. Wazuh has forked it with the purpose of maintaing it. Thank you to the authors for the contribution.
+
+## References
+
+* [Wazuh website](http://wazuh.com)
+* [OSSEC project website](http://ossec.github.io)
---
+++ manifests/params.pp
@@ -0,0 +1,105 @@
+class wazuh::params {
+  case $::kernel {
+    'Linux': {
+
+      $config_file = '/var/ossec/etc/ossec.conf'
+      $config_mode = '0440'
+      $config_owner = 'root'
+      $config_group = 'ossec'
+
+      $keys_file = '/var/ossec/etc/client.keys'
+      $keys_mode = '0440'
+      $keys_owner = 'root'
+      $keys_group = 'ossec'
+
+      $processlist_file = '/var/ossec/bin/.process_list'
+      $processlist_mode = '0440'
+      $processlist_owner = 'root'
+      $processlist_group = 'ossec'
+
+      case $::osfamily {
+        'Debian': {
+
+          $agent_service  = 'wazuh-agent'
+
+          $agent_package  = 'wazuh-agent'
+
+          $service_has_status  = false
+
+          $ossec_service_provider = undef
+
+          $default_local_files = {
+            '/var/log/syslog'             => 'syslog',
+            '/var/log/auth.log'           => 'syslog',
+            '/var/log/mail.log'           => 'syslog',
+            '/var/log/dpkg.log'           => 'syslog',
+            '/var/log/apache2/access.log' => 'apache',
+            '/var/log/apache2/error.log'  => 'apache'
+          }
+
+          case $::lsbdistcodename {
+            /(precise|trusty|vivid|wily|xenial)/: {
+              $server_service = 'wazuh-manager'
+              $server_package = 'wazuh-manager'
+            }
+            /^(jessie|wheezy|stretch|sid)$/: {
+              $server_service = 'wazuh-manager'
+              $server_package = 'wazuh-manager'
+            }
+            default: { fail('This ossec module has not been tested on your distribution (or lsb package not installed)') }
+          }
+
+        }
+        'Redhat': {
+
+          $agent_service  = 'wazuh-agent'
+
+          $agent_package  = 'wazuh-agent'
+
+          $server_service = 'wazuh-manager'
+
+          $server_package = 'wazuh-manager'
+
+          $service_has_status  = true
+
+          $ossec_service_provider = 'redhat'
+
+          $default_local_files = {
+            '/var/log/messages'         => 'syslog',
+            '/var/log/secure'           => 'syslog',
+            '/var/log/maillog'          => 'syslog',
+            '/var/log/yum.log'          => 'syslog',
+            '/var/log/httpd/access_log' => 'apache',
+            '/var/log/httpd/error_log'  => 'apache'
+          }
+
+        }
+      }
+    }
+    'windows': {
+      $config_file = regsubst(sprintf('c:/Program Files (x86)/ossec-agent/ossec.conf'), '\\\\', '/')
+      $config_owner = 'Administrator'
+      $config_group = 'Administrators'
+
+      $keys_file = regsubst(sprintf('c:/Program Files (x86)/ossec-agent/client.keys'), '\\\\', '/')
+      $keys_mode = '0440'
+      $keys_owner = 'Administrator'
+      $keys_group = 'Administrators'
+
+      $agent_service  = 'OssecSvc'
+
+      $agent_package  = 'WAZUH HIDS 1.1.1'
+
+      $server_service = ''
+
+      $server_package = ''
+
+      $service_has_status  = true
+
+      # Pushed by shared agent config now
+      $default_local_files = {}
+
+    }
+    default: { fail('This ossec module has not been tested on your distribution') }
+  }
+}
